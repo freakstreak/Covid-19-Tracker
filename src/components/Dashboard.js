5f649dc8783c6f306b545156e0bf7d24329e5318
Tanmay Jaiswal <tanmayjaiswal2201@gmail.com>
2023-03-17 17:06:53 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className="dashboard">
-          <img src="https://www.un.org/sites/un2.un.org/files/covid19_response_icon.svg" alt=""/>
+          <img src="https://www.un.org/sites/un2.un.org/files/covid19_response_icon.svg" alt="xyz"/>
           <h2>Covid19 Dashboard</h2>
     </div>
   )
